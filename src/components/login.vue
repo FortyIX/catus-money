7c6ac7aa175eb42807279d512e00e8e690b674cc
Fu Zhang <zhangfu2014@live.com>
2021-11-18 11:23:32 +0800

    altered login style

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,24 +1,23 @@
 <template>
 <div class="login-bg">
+    
     <div class="opt-panel">
-        <n-grid x-gap="15" cols="1 700:2">
+    <n-grid x-gap="15" cols="1 700:1">
     <n-gi>
-       <n-card title="登录到您的 Catus Money" hoverable embedded :bordered="false" class="inCard spend-card">
+       <n-card  embedded :bordered="true" class="inCard">
+         <img class="logo-login" width="100" src="https://pic.imgdb.cn/item/61935f462ab3f51d9181aabe.png"/>
            <n-input v-model:value="username" placeholder="账号" style="margin-bottom:20px;"></n-input>
            <n-input v-model:value="password" placeholder="密码" type="password" style="margin-bottom:20px;"></n-input>
            <n-button @click="login" >登录</n-button>
-        </n-card>
-    </n-gi>
-    <n-gi>
-       <n-card title="注册及重置数据库" hoverable embedded :bordered="false" class="inCard" >
-             <n-button style="margin-right:10px;">注册您的帐号</n-button>
+
+          <n-button style="margin-right:10px; margin-left:10px;">注册您的帐号</n-button>
              <n-button>重置数据库</n-button>
             <ul>
               <li>除非您是此系统的第一位用户，之后的用户注册均需第一位用户的密码作为邀请码</li>
               <li>您可以在备份数据库后重置系统来重设第一用户的信息，不过您仍需邀请码</li>
             </ul>
-            
         </n-card>
+
     </n-gi>
     <!-- <n-gi>
        <n-card title="存钱" hoverable embedded :bordered="false" class="inCard">
@@ -76,19 +75,26 @@ export default defineComponent({
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .opt-panel{
-    margin-top:15%;
-    width: 60%;
-    margin-left: 21%;
-    margin-right:15%;
+    margin-top:5%;
+    width:25%;
+    margin-left: 36%;
+    margin-right:40%;
     
 }
 
+.logo-login{
+   margin-left:35%;
+   margin-bottom: 20px;
+}
+
 .inCard{
-    background: rgba(255,255,255, 0.6);
+    background: rgba(255,230,153, 0.1);
+    box-shadow: 10%;
+    border-radius: 25px;
 }
 
 .login-bg{
-     background: url('../assets/login-bg.jpg');
+    
     position:fixed;
     top: 0;
     left: 0;    
